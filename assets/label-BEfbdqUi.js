b01039d2552646c1ee4c515ad4d3d0b47582588f
RenatoFilho <https://github.com/RenatofilhoDevandtech>
2026-01-13 12:03:08 -0300

    Updates

---
+++ assets/label-BEfbdqUi.js
@@ -0,0 +1,1 @@
+import{r,j as t,aH as a,a as i}from"./vendor-DzdZ1USH.js";import{c as l}from"./index-eqsWrvL4.js";const m=i("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),d=r.forwardRef(({className:s,...e},o)=>t.jsx(a,{ref:o,className:l(m(),s),...e}));d.displayName=a.displayName;export{d as L};
